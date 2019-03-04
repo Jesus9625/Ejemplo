9f03ad2582d86b4fd32c07c08d19e17754fcd952
Jesus <jesusma250196@gmail.com>
2019-03-04 03:00:00 -0700

    Vistas y controlador Default

---
+++ Models/EmpleadosModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Ejemplo.Models
+{
+    public class EmpleadoModel
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Centro { get; set; }
+
+        public int Zona { get; set; }
+
+    }
+}
